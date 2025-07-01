57e07c1488b96694c3aaeb40bf8fa12a3375781e
PhongLH <hongphongle774@gmail.com>
2025-07-01 20:51:33 +0700

    test JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,22 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards, Request, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService){}
 
+    @HttpCode(HttpStatus.OK)
     @Post('/login')
+    // @UseGuards(AuthGuard)  // Ap dung cho 1 route cu the
     login(@Body() authDto: AuthDto) {
         return this.authService.signIn(authDto.email, authDto.password);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
     }
 }
